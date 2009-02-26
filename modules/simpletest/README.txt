f1d76811b708d41d5eb0ca4556f0500b8a187e3e
David Strauss <david@fourkitchens.com>
2009-02-26 00:33:44 +0000

    Simpletest 6.x-2.7

--- modules/simpletest/README.txt
+++ modules/simpletest/README.txt
@@ -1,14 +1,28 @@
-$Id: README.txt,v 1.12.4.2 2008/08/15 16:42:21 boombatower Exp $
+$Id: README.txt,v 1.12.4.3 2009/02/08 02:35:39 boombatower Exp $
+
+AUTHOR
+------
+Jimmy Berry ("boombatower", http://drupal.org/user/214218)
+
+PROJECT PAGE
+------------
+If you need more information, have an issue, or feature request please
+visit the project page at: http://drupal.org/project/simpletest.
 
 DESCRIPTION
 -----------
-This mimics the Drupal 7 core simpletest module. The same format may be used in
-this module as in the core Drupal 7 version. Please use any documenation for
-the Drupal 7 version.
+SimpleTest 6.x-2.x is a backport of Drupal 7 core SimpleTest.
+
+STATUS
+------
+Drupal 7 core SimpleTest developement is backported regularly. To find out when
+the code was last backport check the CHANGELOG.txt or version release date.
 
 ISSUES
 ------
-Any issues may be posted to the SimpleTest issue queue
-(http://drupal.org/project/issues/simpletest), but if they are feature requests
-or effect both the core version and the 6.x version then the issue will be
-moved into the core and committed there first.
+If you encounter issues please try and confirm they are Drupal 6 specifc. This
+module has only required changes made in order for it to function in Drupal 6.
+Any issues that are related to the Drupal 6 backport may be posted to:
+http://drupal.org/project/issues/simpletest, but any issues with API or general
+concerns should be posted to the SimpleTest core issue queue:
+http://tinyurl.com/simpletest-core.
--- modules/simpletest/simpletest.js
+++ modules/simpletest/simpletest.js
@@ -1,75 +1,103 @@
-// $Id: simpletest.js,v 1.2.4.3 2008/10/09 05:05:15 boombatower Exp $
+// $Id: simpletest.js,v 1.2.4.4 2009/02/08 02:35:39 boombatower Exp $
 
 /**
  * Add the cool table collapsing on the testing overview page.
  */
+//Drupal.behaviors.simpleTestMenuCollapse = {
+//  attach: function() {
 Drupal.behaviors.simpleTestMenuCollapse = function() {
-  // Adds expand-collapse functionality.
-  $('div.simpletest-image').each(function() {
-    direction = Drupal.settings.simpleTest[$(this).attr('id')].imageDirection;
-    $(this).html(Drupal.settings.simpleTest.images[direction]);
-  });
-  $('div.simpletest-image').click(function() {
-    // Toggle all of the trs.
-    if (!Drupal.settings.simpleTest[$(this).attr('id')].clickActive) {
-      Drupal.settings.simpleTest[$(this).attr('id')].clickActive = true;
-      var trs = $(this).parents('tbody').children().filter('.' + Drupal.settings.simpleTest[$(this).attr('id')].testClass), trs_formatted = [], direction = Drupal.settings.simpleTest[$(this).attr('id')].imageDirection, self = $(this);
-      for (var i = 0; i < trs.length; i++) {
-        trs_formatted.push(trs[i]);
+    var timeout = null;
+    // Adds expand-collapse functionality.
+    $('div.simpletest-image').each(function() {
+      direction = Drupal.settings.simpleTest[$(this).attr('id')].imageDirection;
+      $(this).html(Drupal.settings.simpleTest.images[direction]);
+    });
+
+    // Adds group toggling functionality to arrow images.
+    $('div.simpletest-image').click(function() {
+      var trs = $(this).parents('tbody').children('.' + Drupal.settings.simpleTest[this.id].testClass);
+      var direction = Drupal.settings.simpleTest[this.id].imageDirection;
+      var row = direction ? trs.size() - 1 : 0;
+
+      // If clicked in the middle of expanding a group, stop so we can switch directions.
+      if (timeout) {
+        clearTimeout(timeout);
       }
-      var toggleTrs = function(trs, action, action2) {
-        tr = trs[action]();
-        if (tr) {
-          $(tr)[action2](1, function() {
-            toggleTrs(trs, action, action2);
-          });
+
+      // Function to toggle an individual row according to the current direction.
+      // We set a timeout of 20 ms until the next row will be shown/hidden to
+      // create a sliding effect.
+      function rowToggle() {
+        if (direction) {
+          if (row >= 0) {
+            $(trs[row]).hide();
+            row--;
+            timeout = setTimeout(rowToggle, 20);
+          }
         }
         else {
-          Drupal.settings.simpleTest[self.attr('id')].clickActive = false;
+          if (row < trs.size()) {
+            $(trs[row]).removeClass('js-hide').show();
+            row++;
+            timeout = setTimeout(rowToggle, 20);
+          }
         }
       }
-      toggleTrs(trs_formatted, (direction ? 'pop' : 'shift'), (direction ? 'fadeOut' : 'fadeIn'));
-      Drupal.settings.simpleTest[$(this).attr('id')].imageDirection = !direction;
-      $(this).html(Drupal.settings.simpleTest.images[(direction? 0 : 1)]);
-    }
-  });
-}
+
+      // Kick-off the toggling upon a new click.
+      rowToggle();
+
+      // Toggle the arrow image next to the test group title.
+      $(this).html(Drupal.settings.simpleTest.images[(direction ? 0 : 1)]);
+      Drupal.settings.simpleTest[this.id].imageDirection = !direction;
+
+    });
+//  }
+};
 
 /**
  * Select/deselect all the inner checkboxes when the outer checkboxes are
  * selected/deselected.
  */
+//Drupal.behaviors.simpleTestSelectAll = {
+//  attach: function() {
 Drupal.behaviors.simpleTestSelectAll = function() {
-  $('td.simpletest-select-all').each(function() {
-    var checkboxes = Drupal.settings.simpleTest['simpletest-test-group-'+ $(this).attr('id')].testNames, totalCheckboxes = 0,
-      checkbox = $('<input type="checkbox" class="form-checkbox" id="'+ $(this).attr('id') +'-select-all" />').change(function() {
-      var checked = !!($(this).attr('checked'));
-      for (var i = 0; i < checkboxes.length; i++) {
-        $('#'+ checkboxes[i]).attr('checked', checked);
+    $('td.simpletest-select-all').each(function() {
+      var testCheckboxes = Drupal.settings.simpleTest['simpletest-test-group-' + $(this).attr('id')].testNames;
+      var groupCheckbox = $('<input type="checkbox" class="form-checkbox" id="' + $(this).attr('id') + '-select-all" />');
+
+      // Each time a single-test checkbox is checked or unchecked, make sure
+      // that the associated group checkbox gets the right state too.
+      var updateGroupCheckbox = function() {
+        var checkedTests = 0;
+        for (var i = 0; i < testCheckboxes.length; i++) {
+          $('#' + testCheckboxes[i]).each(function() {
+            if (($(this).attr('checked'))) {
+              checkedTests++;
+            }
+          });
+        }
+        $(groupCheckbox).attr('checked', (checkedTests == testCheckboxes.length));
       }
-      self.data('simpletest-checked-tests', (checked ? checkboxes.length : 0));
-    }).data('simpletest-checked-tests', 0);
-    var self = $(this);
-    for (var i = 0; i < checkboxes.length; i++) {
-      if ($('#' + checkboxes[i]).change(function() {
-        if (checkbox.attr('checked') == 'checked') {
-          checkbox.attr('checked', '');
+
+      // Have the single-test checkboxes follow the group checkbox.
+      groupCheckbox.change(function() {
+        var checked = !!($(this).attr('checked'));
+        for (var i = 0; i < testCheckboxes.length; i++) {
+          $('#'+ testCheckboxes[i]).attr('checked', checked);
         }
-        var data = (!self.data('simpletest-checked-tests') ? 0 : self.data('simpletest-checked-tests')) + (!!($(this).attr('checked')) ? 1 : -1);
-        self.data('simpletest-checked-tests', data);
-        if (data == checkboxes.length) {
-          checkbox.attr('checked', 'checked');
-        }
-        else {
-          checkbox.removeAttr('checked');
-        }
-      }).attr('checked') == 'checked') {
-        totalCheckboxes++;
+      });
+
+      // Have the group checkbox follow the single-test checkboxes.
+      for (var i = 0; i < testCheckboxes.length; i++) {
+        $('#' + testCheckboxes[i]).change(function() {
+          updateGroupCheckbox();
+        });
       }
-    }
-    if (totalCheckboxes == checkboxes.length) {
-      $(checkbox).attr('checked', 'checked');
-    }
-    $(this).append(checkbox);
-  });
+
+      // Initialize status for the group checkbox correctly.
+      updateGroupCheckbox();
+      $(this).append(groupCheckbox);
+    });
+//  }
 };
