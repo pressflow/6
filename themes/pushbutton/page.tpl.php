8278623b80f3abac676992a656a1022d387d9543
bzr <bzr@web3.fourkitchens.com>
2009-04-30 00:36:53 +0000

    Drupal 6.11

--- themes/pushbutton/page.tpl.php
+++ themes/pushbutton/page.tpl.php
@@ -1,11 +1,11 @@
 <?php
-// $Id: page.tpl.php,v 1.25.2.1 2008/09/29 13:32:15 goba Exp $
+// $Id: page.tpl.php,v 1.25.2.2 2009/04/30 00:13:31 goba Exp $
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 <head>
-  <title><?php print $head_title ?></title>
   <meta http-equiv="Content-Style-Type" content="text/css" />
   <?php print $head ?>
+  <title><?php print $head_title ?></title>
   <?php print $styles ?>
   <?php print $scripts ?>
 </head>
